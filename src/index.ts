693e75b6508a06bff1783ae58a65953bbb66dc75
David Sanders <dsanders_m633@outlook.com>
2023-01-27 17:35:47 -0500

    added exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,59 @@
+import useFetch from "./hooks/use-fetch";
+import useCleanupEffect from "./hooks/use-cleanup-effect";
+import useClipboard from "./hooks/use-clipboard";
+import useCSS from "./hooks/use-css";
+import useDataAttributes from "./hooks/use-data-attributes";
+import useDateTime from "./hooks/use-date-time";
+import useDebounce from "./hooks/use-debounce";
+import useExports from "./hooks/use-exports";
+import useHTML from "./hooks/use-html";
+import useInput from "./hooks/use-input";
+import useJSON from "./hooks/use-json";
+import useNonInitialEffect from "./hooks/use-non-initial-effect";
+import useNumber from "./hooks/use-number";
+import useObject from "./hooks/use-object";
+import useQuerySelector from "./hooks/use-query-selector";
+import useRegex from "./hooks/use-regex";
+import useString from "./hooks/use-string";
 
 const Cork = {
-
+    useFetch,
+    useCleanupEffect,
+    useClipboard,
+    useCSS,
+    useDataAttributes,
+    useDateTime,
+    useDebounce,
+    useExports,
+    useHTML,
+    useInput,
+    useJSON,
+    useNonInitialEffect,
+    useNumber,
+    useObject,
+    useQuerySelector,
+    useRegex,
+    useString
 }
 
 export {
-
+    useFetch,
+    useCleanupEffect,
+    useClipboard,
+    useCSS,
+    useDataAttributes,
+    useDateTime,
+    useDebounce,
+    useExports,
+    useHTML,
+    useInput,
+    useJSON,
+    useNonInitialEffect,
+    useNumber,
+    useObject,
+    useQuerySelector,
+    useRegex,
+    useString
 };
 
 export default Cork;
